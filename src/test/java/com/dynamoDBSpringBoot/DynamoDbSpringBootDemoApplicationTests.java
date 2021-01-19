a873eacbef3db3cf57c5bd740478c7f672612dda
shaurya22c <shauryabavishi2016@gmail.com>
2021-01-18 18:53:20 -0600

    CRUD-DynamoDB-SpringBoot

---
+++ src/test/java/com/dynamoDBSpringBoot/DynamoDbSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamoDBSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
